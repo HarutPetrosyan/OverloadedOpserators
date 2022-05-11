8b3e5d2c366a34521b81a33638823725e79de6c9
Harut <43827108+Harut879794@users.noreply.github.com>
2022-05-11 21:58:38 +0400

    Add project files.

---
+++ OverloadedOps/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    internal class Point:IComparable<Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int x,int y)
+        {
+            X = x; Y = y; 
+        }
+        public override string ToString() => $"[{this.X},{this.Y}]";
+        public static Point operator +(Point p1,Point p2)=> new Point((p1.X+p2.X),(p1.Y+p2.Y));
+            
+        
+        public static Point operator -(Point p1, Point p2)=> new Point((p1.X - p2.X), (p1.Y - p2.Y));
+            
+        public static Point operator +(Point p,int number)=>new Point((p.X+number), (p.Y+number)); 
+        public static Point operator +(int number, Point p)=>new Point((number+p.X), (number+ p.Y)); 
+        public static Point operator ++(Point point)=>new Point(point.X+1,point.Y+1);
+        public static Point operator --(Point point)=>new Point(point.X-1, point.Y-1);
+        public override bool Equals(object obj) => ToString() == this.ToString();
+        public override int GetHashCode() => ToString().GetHashCode();
+        public static  bool operator ==(Point p,Point p2)=>p.Equals(p);
+        public static bool operator !=(Point p,Point p2)=>!p.Equals(p2);
+        public int CompareTo(Point other)
+        {
+            if (this.X > other.X && this.Y > other.Y)
+                return 1;
+            if (this.X < other.X && this.Y < other.Y)
+                return -1;
+            else
+                return 0;
+        }
+        public static bool operator >(Point p,Point p2)=>p.CompareTo(p2) > 0;
+        public static bool operator <(Point p,Point p2)=>p.CompareTo(p2) < 0;
+        public static bool operator <= (Point p,Point p2)=>p.CompareTo(p2) <= 0;
+        public static bool operator >= (Point p,Point p2)=>p.CompareTo(p2) >= 0;
+
+    }
+}
---
+++ OverloadedOps/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point P1=new Point(10,10);
+            Point P2=new Point(20,20);
+            Console.WriteLine($"P1+P2: {P1+P2}");
+
+            Console.WriteLine($"P2-P1:{P2-P1}");
+            Console.WriteLine($"P1+12: {P1+12}");
+            Console.WriteLine($"23+P2: {23 + P2}");
+            Console.WriteLine($"p1+=p2:{P1 += P2}");
+            Console.WriteLine($"P2-=P1:{P2-=P1}");
+            Point P3=new Point(30,30);
+            Point p4=new Point(40,40);
+            Console.WriteLine($"++P3: {++P3}");
+            Console.WriteLine($"--P4:{++p4}");
+            Console.WriteLine($"P1==P2: {P1==P2}");
+            Console.WriteLine($"P3==P4:{P3!=p4}");
+        }
+    }
+}
